c8b68a05748743b8dc0f95f7b7b0867355f43a23
AleksanderZarnowski <azarnowski1@gmail.com>
2014-06-09 23:01:47 +0200

    Add GreedyPerturbationInsert algorithm.

--- src/algorithm/AdaptiveLargeNeighbourhoodSearch/RouletteWheel.java
+++ src/algorithm/AdaptiveLargeNeighbourhoodSearch/RouletteWheel.java
@@ -3,6 +3,7 @@ package algorithm.AdaptiveLargeNeighbourhoodSearch;
 import java.util.Random;
 
 import algorithm.AdaptiveLargeNeighbourhoodSearch.insert.GreedyInsert;
+import algorithm.AdaptiveLargeNeighbourhoodSearch.insert.GreedyPerturbedInsert;
 import algorithm.AdaptiveLargeNeighbourhoodSearch.insert.InsertMethod;
 import algorithm.AdaptiveLargeNeighbourhoodSearch.insert.RegretInsert;
 import algorithm.AdaptiveLargeNeighbourhoodSearch.removal.RandomRemoval;
@@ -12,19 +13,40 @@ import algorithm.AdaptiveLargeNeighbourhoodSearch.removal.WorstRemoval;
 
 public class RouletteWheel {
 
-	public static InsertMethod getInsertMethod(){
-		Random r = new Random();
-		int y = r.nextInt(10);
-		if (y < 5) return new RegretInsert();
-		else return new GreedyInsert();
+	private static final int NUMER_OF_ALGORITHMS = 3;
+
+	private static final int REGRET = 0;
+
+	private static final int GREEDY = 1;
+
+	private static final int GREEDY_PERTURBED = 2;
+
+	public static InsertMethod getInsertMethod() {
+		Random random = new Random();
+		int algorithmNumber = random.nextInt(NUMER_OF_ALGORITHMS);
+
+		switch (algorithmNumber) {
+		case REGRET:
+			return new RegretInsert();
+		case GREEDY:
+			return new GreedyInsert();
+		case GREEDY_PERTURBED:
+			return new GreedyPerturbedInsert();
+		default:
+			return null;
+		}
+
 	}
-	
-	public static RemovalMethod getRemovalMethod(){
+
+	public static RemovalMethod getRemovalMethod() {
 		Random r = new Random();
 		int y = r.nextInt(10);
-		if (y < 7) return new ShawRemoval();
-		else if (y < 9) return new WorstRemoval();
-		else return new RandomRemoval();
+		if (y < 7)
+			return new ShawRemoval();
+		else if (y < 9)
+			return new WorstRemoval();
+		else
+			return new RandomRemoval();
 
 	}
 
---
+++ src/algorithm/AdaptiveLargeNeighbourhoodSearch/insert/GreedyPerturbedInsert.java
@@ -0,0 +1,75 @@
+package algorithm.AdaptiveLargeNeighbourhoodSearch.insert;
+
+import jade.core.AID;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import algorithm.Algorithm;
+import algorithm.Schedule;
+import dtp.commission.Commission;
+import dtp.jade.algorithm.agent.AlgorithmAgentParent;
+
+/**
+ * Regular greedy insertion. Only difference is that its cost is perturbed by
+ * factor d in [0.8, 1.2] to introduce randomization.
+ */
+public class GreedyPerturbedInsert implements InsertMethod {
+
+	@Override
+	public boolean Insert(Map<AID, Schedule> holons,
+			List<Commission> commissions, int searchSize,
+			AlgorithmAgentParent agent, int timestamp) {
+
+		Map<AID, Schedule> tmpSolution = holons;
+
+		for (Commission commision : commissions) {
+			AID bestHolon = null;
+			double leastCost = -1;
+
+			for (AID a : tmpSolution.keySet()) {
+				Schedule schedule = holons.get(a);
+				double newCost;
+				Schedule tmp = Schedule.copy(schedule);
+				Algorithm alg = tmp.getAlgorithm();
+				Schedule newSched = alg.makeSchedule(agent, commision, null,
+						tmp, timestamp);
+				if (newSched != null) {
+					newCost = perturb(newSched.calculateCost(newSched
+							.getAlgorithm().getSimInfo()));
+					if (leastCost == -1) {
+						leastCost = newCost;
+						bestHolon = a;
+					} else {
+						if (leastCost > newCost) {
+							leastCost = newCost;
+							bestHolon = a;
+						}
+					}
+				}
+
+			}
+
+			if (bestHolon != null) {
+				Schedule newSched = tmpSolution
+						.get(bestHolon)
+						.getAlgorithm()
+						.makeSchedule(agent, commision, null,
+								tmpSolution.get(bestHolon), timestamp);
+				tmpSolution.put(bestHolon, newSched);
+			} else
+				return false;
+
+		}
+		return true;
+	}
+
+	private double perturb(double cost) {
+		Random random = new Random();
+
+		double factor = (random.nextDouble() * 0.4) + 0.8;
+		return cost * factor;
+	}
+
+}
